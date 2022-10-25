17ccd3568cd75e51d940e658bd0e4aa0864eb7f5
pyk-jc <114495086+pyk-jc@users.noreply.github.com>
2022-10-25 10:20:50 +0100

    added rotation and translate to hull

---
+++ Assets/Tank Model/movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 50.0f;
+    public float turnSpeed = 50.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform t = gameObject.transform;
+
+        float throttle = Input.GetAxis("Vertical");
+        float movement = throttle * speed * Time.deltaTime;
+        t.Translate (0.0f, 0.0f, movement);
+
+        float steering = Input.GetAxis("Horizontal");
+        float turn = steering * turnSpeed * Time.deltaTime;
+        t.Rotate (0.0f, turn, 0.0f);
+
+    }
+}
